6d271c14b8786dd12372c22cf979d5d6e6fcf4c6
j-otterbox <69481329+j-otterbox@users.noreply.github.com>
2024-12-11 15:19:58 -0800

    comment outline for first module

---
+++ main.py
@@ -0,0 +1,8 @@
+# - basic database for samples 
+# - needs CRUD functionality
+# - basic information about the samples is tracked (callout, vendor, style, etc.)
+# - need to track: status, ETA if samples ordered, data gathered
+# - later: transmittals should be able to be created from here
+# - should be able to filter table, group by vendor,
+
+# experiment what you can do with tables in pyqt6 to determine what's possible
