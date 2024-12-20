5adb9109e3b520df3b6f91d7710d9e23300b8650
j-otterbox <69481329+j-otterbox@users.noreply.github.com>
2024-12-20 11:18:17 -0800

    progress on table keyboard navigation

--- main.py
+++ main.py
@@ -13,6 +13,39 @@ import dearpygui.dearpygui as dpg
 # - later: transmittals should be able to be created from here
 # - should be able to filter table, group by vendor,
 
+table_data = {
+    "current_cell": [0,0],
+    "rows": [
+    {
+        "callout": "T-01",
+        "vendor": "Daltile",
+        "style": "Lorem Ipsum",
+        "color": "Beige",
+        "finish": "Polished",
+        "size": "12\"x48\"",
+        "thickness": "8mm",
+    },
+    {
+        "callout": "T-02",
+        "vendor": "Stone Source",
+        "style": "Lorem Ipsum",
+        "color": "Gray",
+        "finish": "Matte",
+        "size": "8\"x8\"",
+        "thickness": "10mm",
+    },
+    {
+        "callout": "T-03",
+        "vendor": "Emser",
+        "style": "Lorem Ipsum",
+        "color": "Absolute Black",
+        "finish": "Honed",
+        "size": "8\"X16\"",
+        "thickness": "9mm",
+    },
+    ]
+}
+
 def get_row_checkbox_ids():
     table_rows = dpg.get_item_children("spec_table")[1]
     checkbox_ids = []
@@ -31,6 +64,12 @@ def toggle_checkbox():
             return
     dpg.configure_item("toggle_select_options", show=False)
 
+def row_drag_handler():
+    print("yo im being dragged")
+
+def row_drop_handler():
+    print("yo ive been dropped")
+
 def add_new_row(spec_data:dict):
     table_row_tags = dpg.get_item_children("spec_table")[1] # col 1  
 
@@ -38,8 +77,12 @@ def add_new_row(spec_data:dict):
 
         for j in range(0, 9):
             if j == 0:
-                dpg.add_text(f"{len(table_row_tags)+1}")
-            elif j == 1: # row num
+                row_idx_item_id = dpg.add_text(f"{len(table_row_tags)+1}", drop_callback=row_drop_handler, payload_type="str")
+
+                with dpg.drag_payload(parent=row_idx_item_id, drag_data="hello world", payload_type="str"):
+                    dpg.add_text("dragging row # TBD")
+
+            elif j == 1: # crow num
                 dpg.add_checkbox(callback=toggle_checkbox)
             else:
                 input_text_tag = dpg.add_input_text(width=-1)
@@ -48,7 +91,8 @@ def add_new_row(spec_data:dict):
 
                 dpg.bind_item_handler_registry(input_text_tag, "table_cell_handler")
 
-    # print(table_row_tags)
+    # if not current_cell:
+    #     current_cell = [0,0]
 
 def deleted_selected_rows():
     checkbox_ids = get_row_checkbox_ids()
@@ -66,6 +110,7 @@ def deleted_selected_rows():
             dpg.set_value(row_idx_cell, row_idx)
             
     toggle_checkbox()
+    dpg.set_value("toggle_all_checkbox", False)
     
 def delete_row(table_row_tag:int):
     row_tags = dpg.get_item_children("spec_table")[1]
@@ -129,7 +174,7 @@ with dpg.window(tag="primary_window"):
         dpg.add_button(label="New Row", callback=add_new_row)
         dpg.add_button(label="Delete Row", callback=delete_row)
 
-    with dpg.table(header_row=True, tag="spec_table") as table_tag:
+    with dpg.table(header_row=True, tag="spec_table", user_data=table_data) as table_tag:
 
         # table columns use child slot 0
         dpg.add_table_column(width_fixed=True)
@@ -149,9 +194,36 @@ with dpg.window(tag="primary_window"):
         # with dpg.item_handler_registry(tag="table_cell_handler"):
         #     dpg.add_item_deactivated_handler(callback=deactivated_handler)
 
-def  log_key_press(e, c):
-    print("arrow key was pressed")
-    print(dpg.get_active_window())
+
+def log_key_press(e, c):
+    """ 
+        table user-data shoulda handle the operation of tracking the current cell internally,
+        will have to get parents(rows), and children (cols) to update the active cell after
+        updating the current cell 
+    """
+
+
+    if dpg.is_key_pressed(dpg.mvKey_Right):
+        print("right arrow pressed")
+
+        x, y = dpg.get_item_user_data("spec_table")["current_cell"]
+    
+        # x value cannot exceed number of columns
+
+    elif dpg.is_key_pressed(dpg.mvKey_Down):
+        print("down arrow pressed")
+
+        # y value cannot exceed number of rows
+
+    elif dpg.is_key_pressed(key=dpg.mvKey_Left):
+        print("left arrow pressed")
+
+        # x value cannot be less than 0
+
+    elif dpg.is_key_pressed(key=dpg.mvKey_Up):
+        print("up arrow pressed")
+
+        # y value cannot be less than 0
 
 with dpg.handler_registry():
     dpg.add_key_press_handler(key=dpg.mvKey_Right, callback=log_key_press)
@@ -169,33 +241,3 @@ dpg.start_dearpygui()
 dpg.destroy_context()
 
 # ==== END MAIN ====
-
-# table_data = [
-#     {
-#         "callout": "T-01",
-#         "vendor": "Daltile",
-#         "style": "Lorem Ipsum",
-#         "color": "Beige",
-#         "finish": "Polished",
-#         "size": "12\"x48\"",
-#         "thickness": "8mm",
-#     },
-#     {
-#         "callout": "T-02",
-#         "vendor": "Stone Source",
-#         "style": "Lorem Ipsum",
-#         "color": "Gray",
-#         "finish": "Matte",
-#         "size": "8\"x8\"",
-#         "thickness": "10mm",
-#     },
-#     {
-#         "callout": "T-03",
-#         "vendor": "Emser",
-#         "style": "Lorem Ipsum",
-#         "color": "Absolute Black",
-#         "finish": "Honed",
-#         "size": "8\"X16\"",
-#         "thickness": "9mm",
-#     },
-# ]
