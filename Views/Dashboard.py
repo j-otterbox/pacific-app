42f70e60299e275b50f4b3aefb400299a0b4f9e3
j-otterbox <69481329+j-otterbox@users.noreply.github.com>
2025-01-09 10:43:50 -0800

    add ui for project item in dashboard project explorer

--- Views/Dashboard.py
+++ Views/Dashboard.py
@@ -1,24 +1,54 @@
 import dearpygui.dearpygui as dpg
 
+def load_assets():
+    width, height, channels, data = dpg.load_image("Assets/pac_c_logo.png")
+
+    with dpg.texture_registry():
+        dpg.add_static_texture(width=width, height=height, default_value=data, tag="pac_c_logo")
+
 class Dashboard:
     def __init__(self):
         with dpg.stage() as self._staging_container_id:
-            
-            with dpg.table():
-                dpg.add_table_column(label="Project", width_fixed=True)
-                dpg.add_table_column(label="G.C.", width_fixed=True)
-                dpg.add_table_column(label="Status")
+            with dpg.table(header_row=False):
+                dpg.add_table_column()
+                dpg.add_table_column(width_fixed=True)
+                dpg.add_table_column()
 
                 with dpg.table_row():
-                    dpg.add_button(label="123 Main St.")
-                    dpg.add_text("W.E. O'Neil")
-                    dpg.add_text("3 Tasks Open, 10 Submittals Left")
+                    with dpg.table_cell():
+                        pass
+                    with dpg.table_cell():
+                        dpg.add_image("pac_c_logo")
+                    with dpg.table_cell():
+                        pass
 
-                with dpg.table_row():
-                    dpg.add_button(label="Glassell Park")
-                    dpg.add_text("Fairfield")
-                    dpg.add_text("2 Tasks Open, 5 Submittals Left")
+            with dpg.group(horizontal=True):
+                with dpg.child_window(height=400, width=325, menubar=True):
+                    with dpg.menu_bar():
+                        dpg.add_menu(label="Project Explorer")
+                        # provide options to sort and filter + search
 
+                    with dpg.collapsing_header(label="GC - Project Name"):
+                        with dpg.group(horizontal=True):
+                            dpg.add_button(label="Open", small=True)
+                            dpg.add_button(label="Delete", small=True)
+                            with dpg.popup(dpg.last_item(), modal=True, mousebutton=dpg.mvMouseButton_Left):
+                                dpg.add_text("Please confirm you want to delete project [PROJECT NAME].")
+                                with dpg.group(horizontal=True, indent=144):
+                                    dpg.add_button(label="Yes", width=50)
+                                    dpg.add_button(label="No", width=50)
+                                # dpg.add_separator()
+                                # dpg.add_checkbox(label="Don't ask me next time")
+                        dpg.add_separator()
+                        dpg.add_text("Samples")
+                        dpg.add_progress_bar(label="Progress Bar", width=-1, default_value=1, overlay="100% Ordered from Vendors")
+                        dpg.add_progress_bar(label="Progress Bar", width=-1, default_value=0.78, overlay="78% Delivered to PC")
+                        dpg.add_progress_bar(label="Progress Bar", width=-1, default_value=0.78, overlay="78% Shipped to GC")
+                        dpg.add_progress_bar(label="Progress Bar", width=-1, default_value=0.56, overlay="56% Approved by GC")
+
+                with dpg.child_window(height=400, menubar=True):
+                    with dpg.menu_bar():
+                        dpg.add_menu(label="What's going on...")
 
     def render_view(self):
         dpg.unstage(self._staging_container_id)
@@ -26,6 +56,7 @@ class Dashboard:
 
 dpg.create_context()
 dpg.create_viewport(title='Pacific Carpets', width=800, height=600, resizable=False)
+load_assets()
 
 with dpg.window(tag="primary_window"):
     Dashboard().render_view()
